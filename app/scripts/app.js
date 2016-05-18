81a0a1211f15e2d5ae11bf733d09269c9d261ed7
Nguyen Duy Kien <nguyenduykien237@gmail.com>
2016-05-18 21:25:54 +0700

    Bai tap 1 gitignore

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name baitap01GitignoreApp
+ * @description
+ * # baitap01GitignoreApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('baitap01GitignoreApp', [
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name baitap01GitignoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the baitap01GitignoreApp
+ */
+angular.module('baitap01GitignoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
